ff4a13e914e9c77d5b1104bd42698eb8da4f37ca
SOLOFOARISON Tolotry Ny Avo Arisaina <62713580+Aris1401@users.noreply.github.com>
2025-03-28 21:18:27 +0300

    feat: liste des factures

--- garage-backend/src/controllers/facture.controller.js
+++ garage-backend/src/controllers/facture.controller.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
+const Facture = require('../model/Intervention/Facture/facture')
+
 // Services
 const FactureService = require('../services/factureService')
+
+const { verifyToken } = require('../middlewares/jwt')
 
 // Telecharger pdf facture
 router.get('/:factureId/download', async (req, res) => {
@@ -20,6 +24,23 @@ router.get('/devis/:devisId/download', async (req, res) => {
     res.setHeader("Content-Disposition", `attachment; filename="${filename}"`);
 
     return res.download(stream.path)
+})
+
+// Liste des factures
+router.get('/', [verifyToken],  async (req, res) => {
+    const factures = await Facture.find().sort({ createdAt: -1 }).populate([
+        {
+            path: "intervention",
+            populate: {
+                path: "utilisateur",
+                select: ["-mot_de_passe", "-documents"]
+            }
+        }
+    ])
+
+    return res.status(200).json({
+        data: factures
+    })
 })
 
 module.exports = router
