4e00e9df788c598d4133ab11719091761902ec8b
SOLOFOARISON Tolotry Ny Avo Arisaina <62713580+Aris1401@users.noreply.github.com>
2025-04-03 21:50:35 +0300

    hotfix

--- garage-frontend/src/app/pages/auth/loginManager.ts
+++ garage-frontend/src/app/pages/auth/loginManager.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { CheckboxModule } from 'primeng/checkbox';
 import { InputTextModule } from 'primeng/inputtext';
@@ -10,19 +10,21 @@ import { AppFloatingConfigurator } from '../../layout/component/app.floatingconf
 
 // Services
 import { AuthentificationService } from '../../_services/auth/authentification.service';
+import { AuthStorageService } from '../../_services/storage/auth-storage.service';
+import { MessageModule } from 'primeng/message';
 
 @Component({
-    selector: 'app-login-manager',
+    selector: 'app-login',
     standalone: true,
-    imports: [ButtonModule, CheckboxModule, InputTextModule, PasswordModule, FormsModule, RouterModule, RippleModule, AppFloatingConfigurator],
+    imports: [ButtonModule, CheckboxModule, InputTextModule, PasswordModule, FormsModule, RouterModule, RippleModule, AppFloatingConfigurator, MessageModule ],
     template: `
         <app-floating-configurator />
         <div class="bg-surface-50 dark:bg-surface-950 flex items-center justify-center min-h-screen min-w-[100vw] overflow-hidden">
             <div class="flex flex-col items-center justify-center">
-                <div style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)">
-                    <div class="w-full bg-surface-0 dark:bg-surface-900 py-20 px-8 sm:px-20" style="border-radius: 53px">
+                <div style="border-radius: 26px; padding: 0.3rem;">
+                    <div class="w-full bg-surface-0 dark:bg-surface-900 py-20 px-8 sm:px-20 shadow-xl" style="border-radius: 23px">
                         <div class="text-center mb-8">
-                            <svg viewBox="0 0 54 40" fill="none" xmlns="http://www.w3.org/2000/svg" class="mb-8 w-16 shrink-0 mx-auto">
+                            <!-- <svg viewBox="0 0 54 40" fill="none" xmlns="http://www.w3.org/2000/svg" class="mb-8 w-16 shrink-0 mx-auto">
                                 <path
                                     fill-rule="evenodd"
                                     clip-rule="evenodd"
@@ -38,19 +40,23 @@ import { AuthentificationService } from '../../_services/auth/authentification.s
                                         fill="var(--primary-color)"
                                     />
                                 </g>
-                            </svg>
-                            <div class="text-surface-900 dark:text-surface-0 text-3xl font-medium mb-4">Welcome to PrimeLand!</div>
-                            <span class="text-muted-color font-medium">Sign in to continue</span>
+                            </svg> -->
+                            <div class="text-surface-900 dark:text-surface-0 text-3xl font-medium mb-4">Se connecter</div>
+                            <!-- <span class="text-muted-color font-medium">Se connecter</span> -->
                         </div>
 
                         <div>
                             <form (submit)="onLogin()" #loginForm="ngForm">
                                 <label for="email1" class="block text-surface-900 dark:text-surface-0 text-xl font-medium mb-2">Email</label>
-                                <input pInputText id="email1" name="email" type="text" placeholder="Email address" class="w-full md:w-[30rem] mb-8" [(ngModel)]="email" value="manager1@gmail.com" />
-    
+                                <input pInputText id="email1" name="email" type="text" placeholder="Email address" class="w-full md:w-[30rem] mb-8" [(ngModel)]="email" value="user1@gmail.com" />
+
                                 <label for="password1" class="block text-surface-900 dark:text-surface-0 font-medium text-xl mb-2">Password</label>
-                                <p-password id="password1" name="password" [(ngModel)]="password" placeholder="Password" [toggleMask]="true" styleClass="mb-4" [fluid]="true" [feedback]="false" value="password" ></p-password>
-    
+                                <p-password id="password1" name="password" [(ngModel)]="password" placeholder="Password" [toggleMask]="true" styleClass="mb-4" [fluid]="true" [feedback]="false" value="password"></p-password>
+
+                                @if (isInvalid) {
+                                    <p-message severity="error" icon="pi pi-times-circle" styleClass="mb-2" closable (onClose)="onMessageClosed()">Mot de passe ou email incorrect.</p-message>
+                                }
+
                                 <!-- <div class="flex items-center justify-between mt-2 mb-8 gap-8">
                                     <div class="flex items-center">
                                         <p-checkbox [(ngModel)]="checked" id="rememberme1" binary class="mr-2"></p-checkbox>
@@ -58,7 +64,7 @@ import { AuthentificationService } from '../../_services/auth/authentification.s
                                     </div>
                                     <span class="font-medium no-underline ml-2 text-right cursor-pointer text-primary">Forgot password?</span>
                                 </div> -->
-                                <p-button label="Sign In" styleClass="w-full" type="submit"></p-button>
+                                <p-button label="Sign In" styleClass="w-full" type="submit" [loading]="isLoading"></p-button>
                             </form>
                         </div>
                     </div>
@@ -71,11 +77,43 @@ export class LoginManager {
     email: string = 'manager1@gmail.com';
     password: string = 'password';
 
-    constructor(private authService: AuthentificationService) {}
+    isInvalid = false
+
+    isLoading : boolean = false
+
+    constructor(
+        private authService: AuthentificationService,
+        private authStorage: AuthStorageService,
+        private router : Router
+    ) {}
 
     onLogin() {
-       this.authService.login(this.email, this.password).subscribe((res) => {
-            console.log(res);
+        this.isLoading = true
+
+        this.authService.login(this.email, this.password).subscribe({
+            next: (response) => {
+                this.authStorage.saveToken(response.token, response.roles, response.nom);
+
+                this.isLoading = false
+
+                // Redirection vers le dashboard
+                if (response.roles[0] == "ROLE_USER") {
+                    this.router.navigate(['/rendez-vous']);
+                } else if (response.roles[0] == "ROLE_MANAGER") {
+                    this.router.navigate(['/manager/tableau-de-bord']);
+                } else {
+                    this.router.navigate(['/liste']);
+                }
+            },
+            error: (error) => {
+                console.log(error);
+                this.isInvalid = true
+                this.isLoading = false
+            }
         });
+    }
+
+    onMessageClosed() {
+        this.isInvalid = false;
     }
 }
