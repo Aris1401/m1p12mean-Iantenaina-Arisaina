983480aa6897692ace306b028a15966bb11829d5
SOLOFOARISON Tolotry Ny Avo Arisaina <62713580+Aris1401@users.noreply.github.com>
2026-08-27 00:28:46 +0400

    env change

--- garage-frontend/src/environments/environment.ts
+++ garage-frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    apiUrl: 'https://m1p12mean-iantenaina-arisaina-production.up.railway.app/api/v1/',
+    apiUrl: 'https://m1p12mean-iantenaina-arisaina-production-fe91.up.railway.app/api/v1/',
     production: false
 };
